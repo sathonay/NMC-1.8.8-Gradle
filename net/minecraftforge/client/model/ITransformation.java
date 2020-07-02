335351cd07f35f7d2c628dee682ae112abe803a0
sathonay <33222808+sathonay@users.noreply.github.com>
2020-07-02 23:53:42 +0200

    use minecraft Matrix4f instead of javax one

--- net/minecraftforge/client/model/ITransformation.java
+++ net/minecraftforge/client/model/ITransformation.java
@@ -1,7 +1,7 @@
 package net.minecraftforge.client.model;
 
-import javax.vecmath.Matrix4f;
 import net.minecraft.util.EnumFacing;
+import net.minecraft.util.Matrix4f;
 
 public interface ITransformation
 {
--- optifine/Reflector.java
+++ optifine/Reflector.java
@@ -7,7 +7,6 @@ import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.List;
 import java.util.Map;
-import javax.vecmath.Matrix4f;
 import net.minecraft.block.Block;
 import net.minecraft.block.properties.IProperty;
 import net.minecraft.block.state.IBlockState;
@@ -169,7 +168,7 @@ public class Reflector
     public static ReflectorMethod ForgeHooks_onLivingSetAttackTarget = new ReflectorMethod(ForgeHooks, "onLivingSetAttackTarget");
     public static ReflectorMethod ForgeHooks_onLivingUpdate = new ReflectorMethod(ForgeHooks, "onLivingUpdate");
     public static ReflectorClass ForgeHooksClient = new ReflectorClass("net.minecraftforge.client.ForgeHooksClient");
-    public static ReflectorMethod ForgeHooksClient_applyTransform = new ReflectorMethod(ForgeHooksClient, "applyTransform", new Class[] {Matrix4f.class, Optional.class});
+    public static ReflectorMethod ForgeHooksClient_applyTransform = new ReflectorMethod(ForgeHooksClient, "applyTransform", new Class[] {net.minecraft.util.Matrix4f.class, Optional.class});
     public static ReflectorMethod ForgeHooksClient_dispatchRenderLast = new ReflectorMethod(ForgeHooksClient, "dispatchRenderLast");
     public static ReflectorMethod ForgeHooksClient_drawScreen = new ReflectorMethod(ForgeHooksClient, "drawScreen");
     public static ReflectorMethod ForgeHooksClient_fillNormal = new ReflectorMethod(ForgeHooksClient, "fillNormal");
