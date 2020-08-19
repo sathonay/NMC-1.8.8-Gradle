137236fcf90d1cb4922060a442fa1396679522f5
Noksio <42706907+iamnoksio@users.noreply.github.com>
2020-08-19 11:36:59 -0400

    Optimize LayerCape

--- net/minecraft/client/renderer/entity/layers/LayerCape.java
+++ net/minecraft/client/renderer/entity/layers/LayerCape.java
@@ -33,20 +33,10 @@ public class LayerCape implements LayerRenderer
             double d4 = (double)(-MathHelper.cos(f * (float)Math.PI / 180.0F));
             float f1 = (float)d1 * 10.0F;
             f1 = MathHelper.clamp_float(f1, -6.0F, 32.0F);
-            float f2 = (float)(d0 * d3 + d2 * d4) * 100.0F;
+            float f2 = (float)Math.max(Math.min((d0 * d3 + d2 * d4) * 100.0F, 165.0F), 0.0F);
             float f3 = (float)(d0 * d4 - d2 * d3) * 100.0F;
-
-            if (f2 < 0.0F)
-            {
-                f2 = 0.0F;
-            }
-
-            if (f2 > 165.0F)
-            {
-                f2 = 165.0F;
-            }
-
             float f4 = entitylivingbaseIn.prevCameraYaw + (entitylivingbaseIn.cameraYaw - entitylivingbaseIn.prevCameraYaw) * partialTicks;
+            
             f1 = f1 + MathHelper.sin((entitylivingbaseIn.prevDistanceWalkedModified + (entitylivingbaseIn.distanceWalkedModified - entitylivingbaseIn.prevDistanceWalkedModified) * partialTicks) * 6.0F) * 32.0F * f4;
 
             if (entitylivingbaseIn.isSneaking())
