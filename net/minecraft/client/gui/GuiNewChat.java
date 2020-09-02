9481c81c591462fa795363c39d358a4227b2a2fe
sathonay <33222808+sathonay@users.noreply.github.com>
2020-09-02 17:22:29 +0200

    :lipstick: Transparent ChatBackground IG

--- net/minecraft/client/gui/GuiNewChat.java
+++ net/minecraft/client/gui/GuiNewChat.java
@@ -79,7 +79,7 @@ public class GuiNewChat extends Gui
                             {
                                 int i2 = 0;
                                 int j2 = -i1 * 9;
-                                drawRect(i2, j2 - 9, i2 + l + 4, j2, l1 / 2 << 24);
+                                if (this.mc.currentScreen instanceof GuiChat) drawRect(i2, j2 - 9, i2 + l + 4, j2, l1 / 2 << 24);
                                 String s = chatline.getChatComponent().getFormattedText();
                                 GlStateManager.enableBlend();
                                 this.mc.fontRendererObj.drawStringWithShadow(s, (float)i2, (float)(j2 - 8), 16777215 + (l1 << 24));
